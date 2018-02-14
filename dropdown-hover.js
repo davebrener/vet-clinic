e5a600866fdc495db678c1b6aab9fa1d96f64ac6
Dave Brener <davebrener@gmail.com>
2018-02-14 09:48:29 -0500

    Changed dropdown-hover.js file and edited CSS file

--- dropdown-hover.js
+++ dropdown-hover.js
@@ -1,4 +1,4 @@
-$(function(){
+/*$(function(){
     function is_touch_device() {
         return 'ontouchstart' in window        // works on most browsers 
         || navigator.maxTouchPoints;       // works on IE10/11 and Surface
@@ -23,4 +23,22 @@ $(function(){
       $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
       $(this).toggleClass('open');
     });
+});
+*/
+
+$(function($) {
+  if($(window).width()>768){
+      $('.navbar .dropdown').hover(function() {
+          $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+
+      }, function() {
+          $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+
+      });
+
+      $('.navbar .dropdown > a').click(function(){
+          location.href = this.href;
+      });
+
+  }
 });
